4d53034fde6bafea7db6d27e6a2e53ae7223dcef
Aleksandr Soloshenko <capcom@soft-c.ru>
2024-09-20 18:00:29 +0700

    [swagger] add some API docs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { TAG_QR } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -18,6 +20,15 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true,
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('CarQRLink web client API')
+    .setDescription('')
+    .setVersion(JSON.parse(require('fs').readFileSync('./package.json', 'utf8')).version)
+    .addTag(TAG_QR, 'QR-code bases operations')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
