141d180acd8bec02212098b0a0ac17fa307bd5c7
Aleksandr Soloshenko <capcom@soft-c.ru>
2024-09-23 18:01:41 +0700

    [link] add captcha

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
 import { AccountsConfig } from './accounts.config';
+import { CaptchaConfig } from './captcha.config';
 import { MessagingConfig } from './messaging.config';
 import { NotificationsConfig } from './notifications.config';
 import { StorageConfig } from './storage.config';
 
 @Module({
     imports: [NestConfigModule.forRoot()],
-    providers: [AccountsConfig, StorageConfig, MessagingConfig, NotificationsConfig],
-    exports: [AccountsConfig, StorageConfig, MessagingConfig, NotificationsConfig],
+    providers: [
+        AccountsConfig,
+        StorageConfig,
+        MessagingConfig,
+        NotificationsConfig,
+        CaptchaConfig
+    ],
+    exports: [
+        AccountsConfig,
+        StorageConfig,
+        MessagingConfig,
+        NotificationsConfig,
+        CaptchaConfig
+    ],
 })
 export class ConfigModule { }
