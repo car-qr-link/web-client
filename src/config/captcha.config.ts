857f16e7c61bb2c6d7b198500bda3ad4d4812ef7
Aleksandr Soloshenko <capcom2me@gmail.com>
2024-09-24 21:01:48 +0700

    [root] prepare order feature

--- src/config/captcha.config.ts
+++ src/config/captcha.config.ts
@@ -4,8 +4,10 @@ import { ConfigService } from "@nestjs/config";
 @Injectable()
 export class CaptchaConfig {
     public readonly serverKey: string;
+    public readonly clientKey: string;
 
     constructor(configService: ConfigService) {
         this.serverKey = configService.get('CAPTCHA__SERVER_KEY', '');
+        this.clientKey = configService.get('CAPTCHA__CLIENT_KEY', '');
     }
 }
